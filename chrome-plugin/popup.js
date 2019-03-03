1c247a4d068328f9ea3223d3cb9205aa2f6bc9d8
Marzuk Rashid <marzukr@gmail.com>
2019-03-03 00:09:56 -0600

    4x4git add -A

--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -1,15 +1,17 @@
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-};
+let page = document.getElementById('imgTable');
+const imageSet = ["images/passimage1.jpg", "images/passimage2.jpg", "images/passimage3.jpg", "images/passimage4.jpg", "images/passimage5.jpg", "images/passimage6.jpg", "images/passimage7.jpg", "images/passimage8.jpg", "images/passimage9.jpg", "images/passimage10.jpg", "images/passimage11.jpg", "images/passimage12.jpg", "images/passimage13.jpg", "images/passimage14.jpg", "images/passimage15.jpg", "images/passimage16.jpg"];
+function constructOptions(imageSet) {
+	for (item in imageSet) {
+        let img = document.createElement('img');
+        let number = 'img'+String(parseInt(item)+1);
+        let data = document.getElementById(number)
+        img.src = imageSet[item];
+		img.addEventListener('click', function() {
+			chrome.storage.sync.set({color: item}, function() {
+				console.log('color is ' + item);
+			})
+		});
+		data.append(img)
+	}
+}
+constructOptions(imageSet);
