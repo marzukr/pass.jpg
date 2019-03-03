b294ffc3ff02893f419dc1b18f9983ce2879dd4a
Edward Lee <superduardus@gmail.com>
2019-03-03 00:05:02 -0600

    GitHub specific Autofill

--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -5,11 +5,23 @@ chrome.storage.sync.get('color', function(data) {
     changeColor.setAttribute('value', data.color);
 });
 
+function GetInputType () {
+	var inputs = document.getElementsByTagName ("input");
+	
+	for (var i = 0; i < inputs.length; i++) {
+		if(inputs[i].type == "password"){
+			return inputs[i];
+		}
+	}
+}
+
 changeColor.onclick = function(element) {
     let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-};
+
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 'document.getElementById("password").value = "' + "BBB" + '";'});
+	});
+
+}
