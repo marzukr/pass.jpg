070e610a840e16a5b4154e7408a29418ffabf7b1
Marzuk Rashid <marzukr@gmail.com>
2019-03-03 07:48:25 -0600

    good redirect options

--- chrome-plugin/options.js
+++ chrome-plugin/options.js
@@ -1,18 +1,19 @@
 let button = document.getElementById('buttnum');
-		button.addEventListener('click', function() {
-			var phone = document.getElementById('textField').value;
-			chrome.storage.sync.set({
-			  sms: phone
-			}, function() {
-			  // Update status to let user know options were saved.
-			  var status = document.getElementById('buttnum');
-				if(document.getElementById('textField').value==""||null){
-					status.textContent = 'What was that?';
-				}else{
-					status.textContent = 'Options saved.'
-				}
-			  setTimeout(function() {
-				status.textContent = 'Secure me!';
-			  }, 750);
-			});
-		});
+button.addEventListener('click', function() {
+    var phone = document.getElementById('textField').value;
+    chrome.storage.sync.set({
+        sms: phone
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('buttnum');
+        if(document.getElementById('textField').value==""||null){
+            status.textContent = 'What was that?';
+        }else{
+            status.textContent = 'Options saved.'
+            window.location.href = `chrome-extension://${chrome.runtime.id}/page2.html`
+        }
+        setTimeout(function() {
+            status.textContent = 'Secure me!';
+        }, 750);
+    });
+});
