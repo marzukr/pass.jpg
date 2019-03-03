661c28c0f720d1f05a2bdb29273ec55c99bda198
Marzuk Rashid <marzukr@gmail.com>
2019-03-03 06:55:19 -0600

    random

--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -7,28 +7,60 @@ const unlockURL = "chrome-extension://mfkmnfhjcbaifnddbaokfegjfoojglim/unlock.ht
 const phoneNumber = "8476822685"
 const API_KEY = "0a90fcfd997324346abb2afc1f8a45d59b2751cdCndhVwFbfI4PoGtL1kcgnlWHo"
 
-let changeColor = document.getElementById('changeColor');
-
 //Aryo Constants:
-let page = document.getElementById('imgTable');
-const imageSet = ["images/passimage1.jpg", "images/passimage2.jpg", "images/passimage3.jpg", "images/passimage4.jpg", "images/passimage5.jpg", "images/passimage6.jpg", "images/passimage7.jpg", "images/passimage8.jpg", "images/passimage9.jpg", "images/passimage10.jpg", "images/passimage11.jpg", "images/passimage12.jpg", "images/passimage13.jpg", "images/passimage14.jpg", "images/passimage15.jpg", "images/passimage16.jpg"];
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
 
-function constructOptions(imageSet) {
-	for (item in imageSet) {
+let randomSize = 16;
+let page = document.getElementById('imgTable');
+
+let shuffledArray = ["images/passimage1.jpg", "images/passimage2.jpg", "images/passimage3.jpg", "images/passimage4.jpg", "images/passimage5.jpg", "images/passimage6.jpg", "images/passimage7.jpg", "images/passimage8.jpg", "images/passimage9.jpg", "images/passimage10.jpg", "images/passimage11.jpg", "images/passimage12.jpg", "images/passimage13.jpg", "images/passimage14.jpg", "images/passimage15.jpg", "images/passimage16.jpg"];
+var shufflePromise = new Promise(function (resolve, reject) {
+    chrome.storage.sync.get(["shuffled", "shuffledArray"], function(data) {
+        console.log(data)
+        if(data.shuffled) {
+            console.log("true");
+            shuffledArray = data.shuffledArray;
+        }
+        else {
+            console.log("false");
+            shuffledArray = shuffle(["images/passimage1.jpg", "images/passimage2.jpg", "images/passimage3.jpg", "images/passimage4.jpg", "images/passimage5.jpg", "images/passimage6.jpg", "images/passimage7.jpg", "images/passimage8.jpg", "images/passimage9.jpg", "images/passimage10.jpg", "images/passimage11.jpg", "images/passimage12.jpg", "images/passimage13.jpg", "images/passimage14.jpg", "images/passimage15.jpg", "images/passimage16.jpg"]);
+            console.log(shuffledArray);
+        }
+        constructOptions(shuffledArray);
+    });
+});
+console.log(shuffledArray);
+
+function constructOptions(shuffledArray) {
+	for (item in shuffledArray) {
         let img = document.createElement('img');
         let number = 'img'+String(parseInt(item)+1);
         let data = document.getElementById(number)
-        img.src = imageSet[item];
+        img.src = shuffledArray[item];
 		img.addEventListener('click', function() {
-			chrome.storage.sync.set({color: item}, function() {
-				console.log('color is ' + item);
-			})
+			console.log('color is ' + item);
 		});
-		data.append(img)
-	}
+        data.append(img)
+    }
+    chrome.storage.sync.set({shuffledArray: shuffledArray, shuffled: true});
 }
-
-constructOptions(imageSet);
 
 const sendText = () => {
     const data = {
@@ -74,11 +106,6 @@ const permaLock = () => {
     document.getElementById('lockWarning').innerText = `Locked permanently. Click link in text to unlock.`;
 }
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
-
 function GetInputType () {
 	var inputs = document.getElementsByTagName ("input");
 	
@@ -97,20 +124,20 @@ chrome.storage.sync.get('permaLock', function(data) {
     if(data.permaLock) permaLock();
 });
 
-changeColor.onclick = function(element) {
-    chrome.storage.sync.get('hasBeenLocked', function(data) {
-        if(data.hasBeenLocked) {
-            chrome.storage.sync.set({ permaLock: true });
-            sendText();
-            permaLock();
-        }
-        else lockout();
-    });
+// changeColor.onclick = function(element) {
+//     chrome.storage.sync.get('hasBeenLocked', function(data) {
+//         if(data.hasBeenLocked) {
+//             chrome.storage.sync.set({ permaLock: true });
+//             sendText();
+//             permaLock();
+//         }
+//         else lockout();
+//     });
   
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.getElementById("password").value = "' + "BBB" + '";'}
-        );
-    });
-};
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         chrome.tabs.executeScript(
+//             tabs[0].id,
+//             {code: 'document.getElementById("password").value = "' + "BBB" + '";'}
+//         );
+//     });
+// };
